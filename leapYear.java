d75ca61efe45ed687cc8894430f16a4b2357f530
namitha029 <123963453+namitha029@users.noreply.github.com>
2023-02-07 22:23:53 +0530

    java practice 07-02-23

---
+++ leapYear.java
@@ -0,0 +1,19 @@
+import java.util.*;
+class leapYear{
+  public static void main(String arg[])
+{
+  System.out.println("Enter an year");
+  Scanner sc=new Scanner(System.in);
+  int year=sc.nextInt();
+  if(year%4==0 && year%100!=0 || year%400==0)
+  {
+    System.out.println(year+"is a leap year");
+  }
+  else
+     { 
+       System.out.println(year+"is not a leap year");
+     }
+
+
+}
+}
