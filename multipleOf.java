55820d29c4f867c78e4c965c6335dd72e014a933
namitha029 <123963453+namitha029@users.noreply.github.com>
2023-02-09 21:13:15 +0530

    java practice 09-02-23

---
+++ multipleOf.java
@@ -0,0 +1,20 @@
+import java.util.*;
+class multipleOf
+{
+  public static void main(String arg[])
+{
+Scanner sc=new Scanner(System.in);
+System.out.println("Enter a number");
+int n=sc.nextInt();
+for(int i=1;i<=10;i++)
+{
+System.out.println(n*i); 
+
+}
+
+
+}
+
+  
+
+}
